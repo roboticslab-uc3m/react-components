594c9963f7151cfc22e80394a3098a3886be6560
jgvictores <jcgvicto@gmail.com>
2024-02-18 12:22:56 +0100

    add VideoComponent.js

---
+++ VideoComponent.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+
+import { Container, Row, Col, Dropdown } from 'react-bootstrap';
+
+function Video({websocketIP, ros}) {
+
+    const [selectedVideoTopicName, setSelectedVideoTopicName] = useState();
+    const [videoTopicNamesJsx, setVideoTopicNamesJsx] = useState();
+
+    useEffect(() => {
+        ros.getTopicsForType("sensor_msgs/msg/Image",
+            (topicNames) => {
+                setSelectedVideoTopicName(topicNames[0]);
+                setVideoTopicNamesJsx(topicNames.map(topicName => <Dropdown.Item eventKey={topicName} key={topicName}>{topicName}</Dropdown.Item>));
+            });
+    }, [ros, setSelectedVideoTopicName, setVideoTopicNamesJsx]);
+
+    return (
+        <Container>
+
+            <Row>
+                <Col>
+                    <h2 tabIndex="0">
+                        Video
+                    </h2>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                    <Dropdown onSelect={(selectedKey) => setSelectedVideoTopicName(selectedKey)}>
+                        <Dropdown.Toggle id="dropdown-basic">
+                            {selectedVideoTopicName}
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {videoTopicNamesJsx}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <img
+                        src={"http://" + websocketIP + ":8080/stream?topic=" + selectedVideoTopicName}
+                        alt={"Image from http://" + websocketIP + ":8080/stream?topic=" + selectedVideoTopicName} />
+                </Col>
+            </Row>
+
+            <Row className="mt-3 mb-5">
+                <Col>
+                    Also see:&nbsp;<a href={"http://" + websocketIP + ":8080"} target="_blank" rel="noopener noreferrer">{"http://" + websocketIP + ":8080"}</a>
+                </Col>
+            </Row>
+            <Row className="mt-5 mb-5"></Row>
+
+        </Container>
+    );
+}
+
+export default Video;
